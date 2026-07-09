e16049e8d291832fdb82c20fa05361835dd87e56
devloper-krishang <developer@krishangtechnolab.com>
2026-07-09 15:15:27 +0530

    update code

--- backend/src/routes/themeRoutes.js
+++ backend/src/routes/themeRoutes.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
 const router = express.Router();
 
-const { saveTheme, getTheme } = require("../controllers/themeController");
-const { authMiddleware } = require("../controllers/UserController");
+const { saveTheme, getTheme } = require('../controllers/themeController');
+const { authMiddleware } = require('../controllers/userController');
 
-router.post("/save-theme", authMiddleware, saveTheme);
+router.post('/save-theme', authMiddleware, saveTheme);
 
-router.get("/get-theme", authMiddleware, getTheme);
+router.get('/get-theme', authMiddleware, getTheme);
 
 module.exports = router;
